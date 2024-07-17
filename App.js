b91d267440ceb151807fbe0801dca6c51f9a3dd8
Obed Nuertey <onuertey1997@gmail.com>
2024-07-17 12:53:32 +0000

    patch: added code for pan

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
   withTiming,
 } from 'react-native-reanimated';
 import {
@@ -14,24 +15,40 @@ import {
 
 export default function App() {
   const pressed = useSharedValue(false);
+  const offset = useSharedValue(0);
 
-  const tap = Gesture.Tap()
-    .onBegin(() => {
-      pressed.value = true;
-    })
-    .onFinalize(() => {
-      pressed.value = false;
-    });
+  const pan = Gesture.Pan()
+  .onBegin(()=>{
+    pressed.value = true;
+  })
+  .onChange((event)=>{
+    offset.value = event.translationX;
+  })
+  .onFinalize(()=>{
+    offset.value = withSpring(0);
+    pressed.value = false;
+  })
+
+  // const tap = Gesture.Tap()
+  //   .onBegin(() => {
+  //     pressed.value = true;
+  //   })
+  //   .onFinalize(() => {
+  //     pressed.value = false;
+  //   });
 
   const animatedStyles = useAnimatedStyle(()=>({
     backgroundColor: pressed.value ? '#FFE04B': '#B58DF1',
-    transform: [{scale: withTiming(pressed.value ? 1.2: 1, {duration: 1000})}]
+    transform: [
+      {translateX: offset.value},
+      {scale: withTiming(pressed.value ? 1.2: 1, {duration: 1000})}
+    ]
   }))
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
-        <GestureDetector gesture={tap}>
+        <GestureDetector gesture={pan}>
           <Animated.View style={[styles.circle, animatedStyles]} />
         </GestureDetector>
       </View>
